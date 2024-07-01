5b3fc8298181b352011848bd094d9cebafa65e4c
Papiya Mazumder <papiyamazumder2@gmail.com>
2024-07-01 13:28:15 +0530

    admin navbar changes

--- React_Job_App/src/App.js
+++ React_Job_App/src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { Nav, Navbar, Container } from "react-bootstrap";
 import { RiAccountCircleFill } from "react-icons/ri";
 import { IoBriefcase, IoLogOut } from "react-icons/io5";
-import { MdOutlinePostAdd } from "react-icons/md";
+import { MdOutlinePostAdd, MdNotificationsActive } from "react-icons/md";
 import Jobs from "./components/Jobs/Jobs";
 import JobDetails from "./components/JobDetails/jobDetails";
 import ApplyFormSubmit from "./components/ApplyForm/ApplyFormSubmit";
@@ -16,6 +16,7 @@ import PrivateRoute from "./Auth/PrivateRoute";
 import Logout from "./pages/Logout/Logout";
 import Display from "./components/Profile/Display";
 import PostJob from "./components/PostJob/PostJob";
+import AdminNotification from "./components/AdminNotification/AdminNotification";
 
 function NavbarLinks() {
   const { isAuthenticated, userRole, logout } = useAuth();
@@ -39,7 +40,10 @@ function NavbarLinks() {
         <>
           <Nav.Link as={Link} to="/postjob" className="nav-link-icons">
             <MdOutlinePostAdd color="white" size={24} />
-            <span className="nav-link-text">Post Job</span>
+            <div className="nav-link-text">Post Job</div>
+          </Nav.Link>
+          <Nav.Link as={Link} to="/notifications" className="nav-link-icons">
+            <MdNotificationsActive color="white" size={22} />
           </Nav.Link>
           <Nav.Link as={Link} to="/logout" onClick={logout} className="nav-link-icons">
             <IoLogOut color="white" size={22} />
@@ -87,6 +91,8 @@ function App() {
               <Route path="/submit" element={<ApplyFormSubmit />} />
               <Route path="/job/:id" element={<JobDetails />} />
               <Route path="/postjob" element={<PostJob />} />
+              <Route path="/adminnotification" element={<AdminNotification />} />
+
             </Route>
           </Routes>
         </Router>
